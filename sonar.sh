3f330c41c1df50059952b719b659d359ee43979b
Fred Jacquet <fred@ljf.ch>
2021-08-25 17:23:22 +0200

    remove as it is internal only

--- sonar.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-sonar-scanner \
-  -Dsonar.projectKey=golang-nbu-exporter \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=https://prod-sqube-srv1 \
-  -Dsonar.login=a577749bb28455d35463e716531a0354c1d7bea2
